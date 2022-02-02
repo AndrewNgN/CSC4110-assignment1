b84896560e1ec4518bb0533523a42753869da099
AndrewNgn <nguyena375@gmail.com>
2022-02-01 19:57:58 -0500

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,6 @@
 // Simple C++ program to display "Hello World" 
 // Header file for input output functions 
+//Author: Andrew Nguyen <fv5774@wayne.edu>
 #include<iostream>  
   
 using namespace std; 
